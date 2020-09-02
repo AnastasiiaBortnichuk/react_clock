774837ef0007165d6c8a5bbfb7910a3e526eb31c
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-09-02 15:51:06 +0300

    Updates

---
+++ precache-manifest.27f059fe00057bb2b8029cb0d0e647a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc344874bc15bded1abbe59bf54d93b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56be7431e3ff5537f031",
+    "url": "./static/css/main.2e58ad2e.chunk.css"
+  },
+  {
+    "revision": "8daccb04a7f88300915b",
+    "url": "./static/js/2.754e2893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.754e2893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56be7431e3ff5537f031",
+    "url": "./static/js/main.7c9e40f1.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
